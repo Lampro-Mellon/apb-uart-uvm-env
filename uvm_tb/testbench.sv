06ba856a37122cae8d030c6a3d32768443a47a71
Zaka Ur Rahman <72543611+zakaurrahman-lm@users.noreply.github.com>
2020-12-01 10:08:48 +0500

    Update testbench.sv

--- uvm_tb/testbench.sv
+++ uvm_tb/testbench.sv
@@ -21,6 +21,19 @@ module tbench_top;
 	             	    .PADDR(vifapb.PADDR),
 	             	    .PRDATA(vifapb.PRDATA)
                     );
+	
+  apbuart_property assertions (
+                              	.PCLK(PCLK),
+                 	        .PRESETn(PRESETn),
+	             	              .PSELx(vifapb.PSELx),
+	             	              .PENABLE(vifapb.PENABLE),
+	             	              .PWRITE(vifapb.PWRITE),
+	             	              .PREADY(vifapb.PREADY),
+	             	              .PSLVERR(vifapb.PSLVERR),
+	             	              .PWDATA(vifapb.PWDATA),
+	             	              .PADDR(vifapb.PADDR),
+	             	              .PRDATA(vifapb.PRDATA)            
+                              );
   
   initial 
   begin 
