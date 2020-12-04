0d680ab07ef5adbfb115b1feab86df160eb1e91b
Muhammad Usman Qadir <usman.qadir@lampromellon.com>
2020-12-04 15:08:14 +0500

    Create top-file

---
+++ sim/csrc/filelist.cu
@@ -0,0 +1,33 @@
+PIC_LD=ld
+
+ARCHIVE_OBJS=
+ARCHIVE_OBJS += _15796_archive_1.so
+_15796_archive_1.so : archive.0/_15796_archive_1.a
+	@$(AR) -s $<
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_15796_archive_1.so --whole-archive $< --no-whole-archive
+	@rm -f $@
+	@ln -sf .//../simv.daidir//_15796_archive_1.so $@
+
+
+
+
+
+O0_OBJS =
+
+$(O0_OBJS) : %.o: %.c
+	$(CC_CG) $(CFLAGS_O0) -c -o $@ $<
+ 
+
+%.o: %.c
+	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
+CU_UDP_OBJS = \
+
+
+CU_LVL_OBJS = \
+SIM_l.o 
+
+MAIN_OBJS = \
+objs/amcQw_d.o 
+
+CU_OBJS = $(MAIN_OBJS) $(ARCHIVE_OBJS) $(CU_UDP_OBJS) $(CU_LVL_OBJS)
+
